b7087256e2097810ab85c285a0c2c39ce57080f4
Om Jee Mishra <93421142+om-jee-m@users.noreply.github.com>
2024-05-16 01:11:19 +0530

    zod schema changes

--- src/zod.ts
+++ src/zod.ts
@@ -23,8 +23,8 @@ const SignInBody = z.object({
 const TaskSchema = z.object({
     subject: z.string().min(3).max(255), // Subject must be a string with a minimum length of 3 and a maximum length of 255 characters
     deadline: z.string(), // Deadline must be a string
-    status: z.enum(["Pending", "Completed", "Overdue"]), // Status must be one of the specified enum values
-    isDeleted: z.boolean(), // isDeleted must be a boolean value
+    status: z.enum(["Pending", "Completed", "Overdue"]).optional(), // Status must be one of the specified enum values
+    isDeleted: z.boolean().optional(), // isDeleted must be a boolean value
     subTask: z.array(z.object({ // SubTask must be an array of objects, each adhering to a specific schema
         subject: z.string().min(3).max(255), // Subtask subject must be a string with a minimum length of 3 and a maximum length of 255 characters
         deadline: z.string(), // Subtask deadline must be a string
@@ -45,7 +45,7 @@ const SubTaskUpdateSchema = z.array(z.object({
     subject: z.string().min().max().optional(), // Subtask subject update is optional
     deadline: z.string().optional(), // Subtask deadline update is optional
     status: z.enum(["Pending", "Completed", "Overdue"]).optional(), // Subtask status update is optional
-    _id: z.string() // Subtask ID is required
+    _id: z.string().optional() // Subtask ID is required
 }));
 
 /**
